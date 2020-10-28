dadb9929841e5fe419ec10cc244a877500898553
gustavkirkholt <kirkholtgustav@gmail.com>
2020-10-28 18:09:36 +0100

    Tilføjet gæt knap og on click metode

--- app/src/main/java/com/example/galgeleg/NytSpil.java
+++ app/src/main/java/com/example/galgeleg/NytSpil.java
@@ -3,7 +3,9 @@ package com.example.galgeleg;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.animation.Animation;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -12,19 +14,15 @@ import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
-public class NytSpil extends AppCompatActivity {
+public class NytSpil extends AppCompatActivity implements View.OnClickListener {
 
     //Variabler
     TextView txwordToBeGuessed;
-    String wordToBeGuessed; //Dette er hele ordet
-    String wordDisplayed; //Dette er order med underscores ved de bogstaver man endnu ikke har gættet
-    char[] wordDisplayedCharArray; //Et character array med ordet, det er nogengange nemmer at arbejde med
-    ArrayList<String> muligeOrd = new ArrayList();  //Alle mulige ord
     EditText userInput;
     TextView nameOfUser;
     TextView txLettersTried;
-    String lettersTried;
     ImageView hangMan; //Galjen
+    Button guess;
     final String MESSAGE_WITH_LETTERS_TRIED = "Letters tried: ";
 
     //Nogle animationer
@@ -37,19 +35,27 @@ public class NytSpil extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nyt_spil);
 
-        NytSpil nytspil = new NytSpil();
         GalgeLogik logik = new GalgeLogik();
+        logik.startNytSpil();
 
+        guess = findViewById(R.id.guessLetter);
         nameOfUser = findViewById(R.id.userName);
         txwordToBeGuessed = findViewById(R.id.wordToGuess);
         userInput = findViewById(R.id.userInput);
         txLettersTried = findViewById(R.id.lettersUsed);
         hangMan = findViewById(R.id.gallowStart);
 
+        guess.setOnClickListener(this);
 
 
 
+    }
 
+    @Override
+    public void onClick(View ButtonClick) {
+        if (ButtonClick == guess){
+
+        }
 
     }
 }
