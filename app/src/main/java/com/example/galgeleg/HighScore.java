f2591c62c00e05b9e9b839592550ba57f6891516
gustavkirkholt <kirkholtgustav@gmail.com>
2020-10-30 10:02:13 +0100

    tilføjet get metoder i highscore aktivitet

--- app/src/main/java/com/example/galgeleg/HighScore.java
+++ app/src/main/java/com/example/galgeleg/HighScore.java
@@ -3,6 +3,8 @@ package com.example.galgeleg;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.widget.TextView;
 
 public class HighScore extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class HighScore extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_high_score);
+
+        String username = PreferenceManager.getDefaultSharedPreferences(this).getString("Brugernavn", "defaultStringIfNothingFound");
+
+        String ordet = PreferenceManager.getDefaultSharedPreferences(this).getString("Ordet", "defaultStringIfNothingFound");
+
+        int score = PreferenceManager.getDefaultSharedPreferences(this).getInt("Score", 0);
+
     }
 }
