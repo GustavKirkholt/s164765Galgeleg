a20fca4825480268ed960354e43c16a17defe413
gustavkirkholt <kirkholtgustav@gmail.com>
2020-11-02 09:42:51 +0100

    getOrdet opdateret

--- app/src/main/java/com/example/galgeleg/ordListe.java
+++ app/src/main/java/com/example/galgeleg/ordListe.java
@@ -1,13 +1,20 @@
 package com.example.galgeleg;
 
 import java.util.ArrayList;
+import java.util.Random;
 
-    public abstract class ordListe {
 
-        ArrayList<String> ordet = new ArrayList();
+public abstract class ordListe {
 
-        public ArrayList<String> getOrdet() {
-            return ordet;
+        ArrayList<String> ordListe = new ArrayList();
+        Random randomGenerator;
+
+        public String getOrdet() {
+
+            randomGenerator = new Random();
+            int index = randomGenerator.nextInt(ordListe.size());
+            String ordet = ordListe.get(index);
+            return (ordet);
         }
     }
 
