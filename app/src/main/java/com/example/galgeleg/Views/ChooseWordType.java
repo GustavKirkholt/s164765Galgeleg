88cadbe6fdc137a9c7b200f8487e2b68f84fb0eb
gustavkirkholt <kirkholtgustav@gmail.com>
2021-01-02 21:36:21 +0100

    Tilføjet DropDown menu til at vælge hvilken type ord

--- app/src/main/java/com/example/galgeleg/Views/ChooseWordType.java
+++ app/src/main/java/com/example/galgeleg/Views/ChooseWordType.java
@@ -3,49 +3,64 @@ package com.example.galgeleg.Views;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Spinner;
 
+import com.example.galgeleg.MainActivity;
 import com.example.galgeleg.NemmeOrd;
 import com.example.galgeleg.R;
 import com.example.galgeleg.SværeOrd;
 
-public class ChooseWordType extends AppCompatActivity implements View.OnClickListener{
+import java.util.ArrayList;
 
-    Button knap1;
-    Button knap2;
 
+public class ChooseWordType extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_word_type);
 
-        knap1 = findViewById(R.id.nemtOrd);
-        knap2 = findViewById(R.id.sværtOrd);
+        final ArrayList<String> valgmuligheder = new ArrayList<>();
+        valgmuligheder.add("Nemt");
+        valgmuligheder.add("Svært");
+        valgmuligheder.add("Hent fra DR");
 
-        knap1.setOnClickListener(this);
-        knap2.setOnClickListener(this);
+
+        spinner = findViewById(R.id.spinner);
+        spinner.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, valgmuligheder));
+
+        spinner.setOnItemSelectedListener(this);
+
 
     }
 
     @Override
-    public void onClick(View ClickButton) {
-        if(ClickButton == knap1){
-
-            Intent i = new Intent(this, NytSpil.class);
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        if (position == 0) {
+            Intent i = new Intent(ChooseWordType.this, NytSpil.class);
             i.putExtra("ord", (new NemmeOrd()).getOrdet());
             startActivity(i);
-
-        } else if (ClickButton == knap2){
-
+        } if (position == 1) {
             Intent i = new Intent(this, NytSpil.class);
             i.putExtra("ord", (new SværeOrd()).getOrdet());
             startActivity(i);
-
-
+        } if (position == 2){
+            //TODO Hente ord fra DR
         }
+    }
 
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        return;
     }
 }
