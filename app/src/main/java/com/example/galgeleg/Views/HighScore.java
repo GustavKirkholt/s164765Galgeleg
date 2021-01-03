7c3c16c1776249ba9c150ff6596fdb128af6f9e4
gustavkirkholt <kirkholtgustav@gmail.com>
2021-01-03 18:23:27 +0100

    Tilføjet highscore metoder

--- app/src/main/java/com/example/galgeleg/Views/HighScore.java
+++ app/src/main/java/com/example/galgeleg/Views/HighScore.java
@@ -1,7 +1,10 @@
 package com.example.galgeleg.Views;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.View;
@@ -9,20 +12,32 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
 
+import com.example.galgeleg.R;
+
+import java.util.ArrayList;
 import java.util.Collections;
 
-public class HighScore extends AppCompatActivity {
+public class HighScore extends AppCompatActivity implements R {
 
+    private ArrayList<String> highscore = new ArrayList<>();
+    private SharedPreferences prefs;
+    MyRecipeListAdapter recyclerViewAdapter;
 
-    //@Override
-    //protected void onCreate(Bundle savedInstanceState) {
-    //super.onCreate(savedInstanceState);
-// setContentView(R.layout.activity_high_score);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_high_score);
 
-    // String username = PreferenceManager.getDefaultSharedPreferences(this).getString("Brugernavn", "defaultStringIfNothingFound");
+        prefs = PreferenceHelper.getApplicationPreferences(this);
+        updateListOfBrews();
 
-    //String ordet = PreferenceManager.getDefaultSharedPreferences(this).getString("Ordet", "defaultStringIfNothingFound");
+        RecyclerView listView = this.findViewById(R.id.myrecipes_List);
+        listView.setLayoutManager(new LinearLayoutManager(this));
+        listView.scrollToPosition(0);
 
-    // int score = PreferenceManager.getDefaultSharedPreferences(this).getInt("Score", 0);
+        recyclerViewAdapter = new MyRecipeListAdapter(brews, this);
 
+        listView.setAdapter(recyclerViewAdapter);
+
+    }
 }
