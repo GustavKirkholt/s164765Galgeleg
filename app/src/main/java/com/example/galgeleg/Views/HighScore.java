fe7293dd7d6f377a4c6deb44bee9a1feecc4abc1
gustavkirkholt <kirkholtgustav@gmail.com>
2021-01-03 21:00:28 +0100

    Tilføjet tilbage knap til highscore

--- app/src/main/java/com/example/galgeleg/Views/HighScore.java
+++ app/src/main/java/com/example/galgeleg/Views/HighScore.java
@@ -4,21 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.GridView;
 import android.widget.ListView;
 
+import com.example.galgeleg.MainActivity;
 import com.example.galgeleg.R;
 
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class HighScore extends AppCompatActivity implements {
+public class HighScore extends AppCompatActivity implements View.OnClickListener {
+
+    Button tilbage;
 
     private ArrayList<String> usernames = new ArrayList<>();
     private ArrayList<Integer> scores = new ArrayList<>();
@@ -27,7 +32,6 @@ public class HighScore extends AppCompatActivity implements {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_high_score);
 
         String username = PreferenceManager.getDefaultSharedPreferences(this).getString("Brugernavn", "defaultStringIfNothingFound");
         int score = PreferenceManager.getDefaultSharedPreferences(this).getInt("Score", 0);
@@ -35,14 +39,30 @@ public class HighScore extends AppCompatActivity implements {
         this.usernames.add(username);
         this.scores.add(score);
 
+        adapter = () //indsæt min recyclerview adapter her.
 
-        RecyclerView listView = this.findViewById(R.id.highScore);
+        RecyclerView listView = this.findViewById(R.id.highscore);
         listView.setLayoutManager(new LinearLayoutManager(this));
         listView.scrollToPosition(0);
 
-        recyclerViewAdapter = new MyRecipeListAdapter(usernames, scores, this);
+        listView.setAdapter(); = new RecyclerView.(usernames, scores, this);
 
-        listView.setAdapter(recyclerViewAdapter);
+        listView.setAdapter(adapter);
 
+        setContentView(listView);
+
+        tilbage = findViewById(R.id.tilbagehighscore);
+        tilbage.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View ButtonClick) {
+        if(ButtonClick == tilbage){
+            Intent i = new Intent(this, MainActivity.class);
+            finishAffinity();
+            startActivity(i);
+        }
     }
 }
