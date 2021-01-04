7cdfc81be28cbff6437caa94362baf36922f5b74
gustavkirkholt <kirkholtgustav@gmail.com>
2021-01-04 15:43:41 +0100

    Ordet hentes fra pref manager

--- app/src/main/java/com/example/galgeleg/Views/HighScore.java
+++ app/src/main/java/com/example/galgeleg/Views/HighScore.java
@@ -20,7 +20,7 @@ import com.example.galgeleg.R;
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class HighScore extends AppCompatActivity implements View.OnClickListener {
+public class HighScore extends AppCompatActivity {
 
     Button tilbage;
 
@@ -33,71 +33,11 @@ public class HighScore extends AppCompatActivity implements View.OnClickListener
 
         String username = PreferenceManager.getDefaultSharedPreferences(this).getString("Brugernavn", "defaultStringIfNothingFound");
         int score = PreferenceManager.getDefaultSharedPreferences(this).getInt("Score", 0);
-
+        String ordet = PreferenceManager.getDefaultSharedPreferences(this).getString("Ordet", "defaultStringIfNothingFound");
 
         this.highscores.add(username);
         this.highscores.add(String.valueOf(score));
+        this.highscores.add(ordet);
 
-        adapter = () //indsæt min recyclerview adapter her.
-
-        RecyclerView listView = this.findViewById(R.id.highscore);
-        listView.setLayoutManager(new LinearLayoutManager(this));
-        listView.scrollToPosition(0);
-
-        listView.setAdapter(ListeelemAdapter) = new RecyclerView.(usernames, scores, this);
-
-        listView.setAdapter(ListeelemAdapter);
-
-        setContentView(listView);
-
-        tilbage = findViewById(R.id.tilbagehighscore);
-        tilbage.setOnClickListener(this);
-
-
-    }
-
-    @Override
-    public void onClick(View ButtonClick) {
-            if (ButtonClick == tilbage) {
-                Intent i = new Intent(this, MainActivity.class);
-                finishAffinity();
-                startActivity(i);
-    }
-
-    class ListeelemAdapter extends RecyclerView.Adapter<ListeelemViewholder> {
-        @Override
-        public int getItemCount() {
-            return highscores.size();
-        }
-
-        @Override
-        public ListeelemViewholder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View listeelementViews = getLayoutInflater().inflate(R.layout.activity_high_score, parent, false);
-            ListeelemViewholder vh = new ListeelemViewholder(listeelementViews);
-            return vh;
-        }
-
-        @Override
-        public void onBindViewHolder(ListeelemViewholder vh, int position) {
-            vh.overskrift.setText(highscores.get(position));
-            if (position > 0) vh.overskrift.append(" (flyt op)");
-
-        }
-    }
-
-    ;
-
-    class ListeelemViewholder extends RecyclerView.ViewHolder {
-        TextView overskrift;
-
-        public ListeelemViewholder(View listeelementViews) {
-            super(listeelementViews);
-            overskrift = listeelementViews.findViewById(R.id.listeelem_overskrift);
-            // Gør listeelementer klikbare og vis det ved at deres baggrunsfarve ændrer sig ved berøring
-            overskrift.setBackgroundResource(android.R.drawable.list_selector_background);
-            overskrift.setOnClickListener(this);
-
-        }
-        }
     }
 }
